0c7ca4094c7ee8795619c5136b799e129f9ec887
FernandoVelcic <fernando_velcic_77@hotmail.com>
2014-10-04 05:49:56 +0200

    Esto es un ORM, los nombres de los atributos en los modelos tienen que coincidir con las columnas!

--- src/FrbaHotel/ABM de Habitacion/Form1.cs
+++ src/FrbaHotel/ABM de Habitacion/Form1.cs
@@ -23,12 +23,13 @@ namespace FrbaHotel.ABM_de_Habitacion
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //TODO matchear bien esto
             Habitaciones habitacionNew = new Habitaciones();
             habitacionNew.numero = Convert.ToInt32(textBox1.Text);
-            habitacionNew.hotel= comboBox1.Text;
+            /*habitacionNew.hotel= comboBox1.Text;
             habitacionNew.pisoHotel = comboBox2.Text ;
             habitacionNew.ubicacionEnElHotel = textBox2.Text;
-            habitacionNew.tipoDeHabitacion = comboBox3.Text;
+            habitacionNew.tipoDeHabitacion = comboBox3.Text;*/
             habitacionNew.descripcion = textBox3.Text;
         }
 
--- src/FrbaHotel/Database Helper/Modelos/Habitaciones.cs
+++ src/FrbaHotel/Database Helper/Modelos/Habitaciones.cs
@@ -11,11 +11,14 @@ namespace FrbaHotel
     {
         public override String table { get { return "habitaciones"; } }
 
-        public int numero { get; set; } //id?
-        public String hotel { get; set; } //lo usamos string?
-        public String pisoHotel { get; set; } //idem arriba
-        public String ubicacionEnElHotel { get; set; } //[nvarchar](255)
-        public String tipoDeHabitacion { get; set; } //[nvarchar](255)
+        //public int id { get; set; } //INTEGER IDENTITY PRIMARY KEY
+        public int hotel_id { get; set; } //INTEGER
+        public int numero { get; set; } //[numeric](18, 0)
+        public int piso { get; set; } //[numeric](18, 0)
+        public String frente { get; set; } //[nvarchar](50)
+        public int tipo_codigo { get; set; } //[numeric](18, 0)
+        public String tipo_descripcion { get; set; } //[nvarchar](255)
         public String descripcion { get; set; } //[nvarchar](255)
+        public int tipo_porcentual { get; set; } //[numeric](18, 2)
     }
 }
