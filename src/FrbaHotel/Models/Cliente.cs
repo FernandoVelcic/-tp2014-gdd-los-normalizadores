762778afbb170796214abd773a0db5c78564d44a
FernandoVelcic <fernando_velcic_77@hotmail.com>
2014-10-27 23:12:23 +0100

    Agregando validacion de tipo y numero de documento para ABM clientes

--- src/FrbaHotel/Models/Cliente.cs
+++ src/FrbaHotel/Models/Cliente.cs
@@ -37,15 +37,32 @@ namespace FrbaHotel.Models
         public TipoDocumento documento_tipo { get; set; }
         public long documento_nro { get; set; } //[numeric](18, 0)
 
-        public override void preInsert()
+        public override void preSave()
         {
             if (mail.isValidEmail() != true)
                 throw new ValidationException("Formato de email invalido");
+        }
 
+        public override void preInsert()
+        {
             List<Cliente> clientes = EntityManager.getEntityManager().findAllBy<Cliente>("mail", mail);
             if (clientes.Count != 0)
             {
                 throw new ValidationException("Email duplicado");
+            }
+
+            List<FetchCondition> condiciones_repetidos = new List<FetchCondition>();
+            FetchCondition condicion_documento_tipo = new FetchCondition();
+            condicion_documento_tipo.setEquals("documento_tipo_id", documento_tipo.id.ToString());
+            FetchCondition condicion_documento_nro = new FetchCondition();
+            condicion_documento_nro.setEquals("documento_nro", documento_nro.ToString());
+            condiciones_repetidos.Add(condicion_documento_tipo);
+            condiciones_repetidos.Add(condicion_documento_nro);
+
+            List<Cliente> clientes_repetidos = EntityManager.getEntityManager().findList<Cliente>(condiciones_repetidos);
+            if (clientes_repetidos.Count != 0)
+            {
+                throw new ValidationException("Este cliente ya se encontraba registrado con anterioridad");
             }
         }
     }
