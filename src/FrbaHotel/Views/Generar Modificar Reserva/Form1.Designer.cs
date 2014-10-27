0b2f6934de9ce4567cd87724d0efcfcce9344c74
nicolasazrak <azrak.nicolas@gmail.com>
2014-10-27 18:10:03 -0300

    Mejorando el alta de reserva

--- src/FrbaHotel/Views/Generar Modificar Reserva/Form1.Designer.cs
+++ src/FrbaHotel/Views/Generar Modificar Reserva/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmb_Habitacion = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmb_TipoHabitacion = new System.Windows.Forms.ComboBox();
             this.txt_Cant_Noches = new System.Windows.Forms.TextBox();
             this.txt_Desde = new System.Windows.Forms.TextBox();
             this.cmb_Regimen = new System.Windows.Forms.ComboBox();
@@ -38,12 +41,13 @@
             this.label2 = new System.Windows.Forms.Label();
             this.btn_Generar = new System.Windows.Forms.Button();
             this.btn_Volver = new System.Windows.Forms.Button();
-            this.cmb_TipoHabitacion = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.cmb_Habitacion);
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.cmb_TipoHabitacion);
             this.groupBox1.Controls.Add(this.txt_Cant_Noches);
             this.groupBox1.Controls.Add(this.txt_Desde);
@@ -60,16 +64,45 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos de la reserva";
             // 
+            // cmb_Habitacion
+            // 
+            this.cmb_Habitacion.FormattingEnabled = true;
+            this.cmb_Habitacion.Location = new System.Drawing.Point(174, 168);
+            this.cmb_Habitacion.Name = "cmb_Habitacion";
+            this.cmb_Habitacion.Size = new System.Drawing.Size(121, 24);
+            this.cmb_Habitacion.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Habitacion: ";
+            // 
+            // cmb_TipoHabitacion
+            // 
+            this.cmb_TipoHabitacion.FormattingEnabled = true;
+            this.cmb_TipoHabitacion.Location = new System.Drawing.Point(174, 107);
+            this.cmb_TipoHabitacion.Name = "cmb_TipoHabitacion";
+            this.cmb_TipoHabitacion.Size = new System.Drawing.Size(121, 24);
+            this.cmb_TipoHabitacion.TabIndex = 8;
+            this.cmb_TipoHabitacion.SelectedIndexChanged += new System.EventHandler(this.update_habitaciones);
+            this.cmb_TipoHabitacion.SelectionChangeCommitted += new System.EventHandler(this.update_habitaciones);
+            this.cmb_TipoHabitacion.TextUpdate += new System.EventHandler(this.update_habitaciones);
+            // 
             // txt_Cant_Noches
             // 
-            this.txt_Cant_Noches.Location = new System.Drawing.Point(167, 97);
+            this.txt_Cant_Noches.Location = new System.Drawing.Point(174, 76);
             this.txt_Cant_Noches.Name = "txt_Cant_Noches";
             this.txt_Cant_Noches.Size = new System.Drawing.Size(121, 22);
             this.txt_Cant_Noches.TabIndex = 7;
             // 
             // txt_Desde
             // 
-            this.txt_Desde.Location = new System.Drawing.Point(168, 67);
+            this.txt_Desde.Location = new System.Drawing.Point(175, 46);
             this.txt_Desde.Name = "txt_Desde";
             this.txt_Desde.Size = new System.Drawing.Size(121, 22);
             this.txt_Desde.TabIndex = 7;
@@ -77,16 +110,19 @@
             // cmb_Regimen
             // 
             this.cmb_Regimen.FormattingEnabled = true;
-            this.cmb_Regimen.Location = new System.Drawing.Point(168, 159);
+            this.cmb_Regimen.Location = new System.Drawing.Point(175, 138);
             this.cmb_Regimen.Name = "cmb_Regimen";
             this.cmb_Regimen.Size = new System.Drawing.Size(121, 24);
             this.cmb_Regimen.TabIndex = 5;
+            this.cmb_Regimen.SelectedIndexChanged += new System.EventHandler(this.update_habitaciones);
+            this.cmb_Regimen.SelectionChangeCommitted += new System.EventHandler(this.update_habitaciones);
+            this.cmb_Regimen.TextUpdate += new System.EventHandler(this.update_habitaciones);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(46, 159);
+            this.label5.Location = new System.Drawing.Point(53, 138);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(81, 16);
             this.label5.TabIndex = 4;
@@ -96,7 +132,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(46, 128);
+            this.label4.Location = new System.Drawing.Point(53, 107);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(90, 16);
             this.label4.TabIndex = 3;
@@ -106,7 +142,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(45, 97);
+            this.label3.Location = new System.Drawing.Point(52, 76);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(92, 16);
             this.label3.TabIndex = 2;
@@ -116,7 +152,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(46, 70);
+            this.label2.Location = new System.Drawing.Point(53, 49);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(43, 16);
             this.label2.TabIndex = 1;
@@ -143,14 +179,6 @@
             this.btn_Volver.Text = "Volver";
             this.btn_Volver.UseVisualStyleBackColor = true;
             this.btn_Volver.Click += new System.EventHandler(this.btn_Volver_Click);
-            // 
-            // cmb_TipoHabitacion
-            // 
-            this.cmb_TipoHabitacion.FormattingEnabled = true;
-            this.cmb_TipoHabitacion.Location = new System.Drawing.Point(167, 128);
-            this.cmb_TipoHabitacion.Name = "cmb_TipoHabitacion";
-            this.cmb_TipoHabitacion.Size = new System.Drawing.Size(121, 24);
-            this.cmb_TipoHabitacion.TabIndex = 8;
             // 
             // Form1
             // 
@@ -183,5 +211,7 @@
         private System.Windows.Forms.Button btn_Generar;
         private System.Windows.Forms.Button btn_Volver;
         private System.Windows.Forms.ComboBox cmb_TipoHabitacion;
+        private System.Windows.Forms.ComboBox cmb_Habitacion;
+        private System.Windows.Forms.Label label1;
     }
 }
--- src/FrbaHotel/Views/Generar Modificar Reserva/Form1.cs
+++ src/FrbaHotel/Views/Generar Modificar Reserva/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using FrbaHotel.Database_Helper;
 using FrbaHotel.Models;
 using FrbaHotel.Models.Exceptions;
+using MyActiveRecord;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -43,6 +44,57 @@ namespace FrbaHotel.Generar_Modificar_Reserva
 
             txt_Cant_Noches.DataBindings.Add("Text", this.reserva, "cant_noches");
             txt_Desde.DataBindings.Add("Text", this.reserva, "fecha_inicio");
+            load_Habitaciones();
+        }
+
+        public void update_habitaciones(object sender, EventArgs e)
+        {
+            load_Habitaciones();
+        }
+
+        private void load_Habitaciones()
+        {
+
+            SelectQuery<Habitacion> query = new SelectQuery<Habitacion>(typeof(Habitacion));
+            query.addSelect("habitaciones.id");
+            query.addSelect("habitaciones.numero");
+            query.addSelect("habitaciones.piso");
+            query.addSelect("hoteles.calle");
+            query.addSelect("hoteles.nombre");
+            query.addInnerJoin("hoteles", "habitaciones.hotel_id = hoteles.id");
+            query.addInnerJoin("habitaciones_tipos", "habitaciones.tipo_id = habitaciones_tipos.id");
+            query.addInnerJoin("hoteles_regimenes", "hoteles.id = hoteles_regimenes.hotel_id");
+            query.addInnerJoin("regimenes", "hoteles_regimenes.regimen_id = regimenes.id");
+
+            query.addWhere("regimenes.id", ((Regimen)cmb_Regimen.SelectedItem).id.ToString());
+            query.addWhere("habitaciones.tipo_id", ((TipoHabitacion)cmb_TipoHabitacion.SelectedItem).id.ToString());
+
+            var sql = query.build();
+            SqlCommand command = new SqlCommand(sql, ConnectionManager.getInstance().getConnection());
+            List<Habitacion> habitaciones = new List<Habitacion>();
+            using (SqlDataReader result = command.ExecuteReader())
+            {
+                while (result.Read())
+                {
+                    Habitacion habitacion = new Habitacion();
+                    habitacion.id = Convert.ToInt64(result["id"]);
+                    habitacion.numero = Int32.Parse(result["numero"].ToString(), null);
+                    habitacion.piso = Int32.Parse(result["piso"].ToString(), null);
+
+                    Hotel hotel = new Hotel();
+                    hotel.calle = result["calle"].ToString();
+                    hotel.nombre = result["nombre"].ToString();
+
+                    habitacion.hotel = hotel;
+
+                    habitaciones.Add(habitacion);
+                }
+            }
+
+            BindingSource habitacion_binding = new BindingSource();
+            habitacion_binding.DataSource = habitaciones;
+            cmb_Habitacion.DataSource = habitacion_binding;
+
         }
 
         private void btn_Generar_Click(object sender, EventArgs e)
@@ -72,6 +124,12 @@ namespace FrbaHotel.Generar_Modificar_Reserva
         {
             //TODO: Volver a formulario anterior
         }
+
+        private void update_habitaciones()
+        {
+
+        }
+
 
     }
 }
