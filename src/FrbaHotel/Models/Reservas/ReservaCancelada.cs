af18c6336233a845192cfb26d58e7eff7980c8d4
Diego Fiorillo <diegogfiorillo@hotmail.com>
2014-11-22 22:48:57 -0300

    pre save reserva cancelada

--- src/FrbaHotel/Models/Reservas/ReservaCancelada.cs
+++ src/FrbaHotel/Models/Reservas/ReservaCancelada.cs
@@ -5,6 +5,7 @@ using System.Text;
 using MyActiveRecord;
 using FrbaHotel.Database_Helper;
 using FrbaHotel.Models;
+using FrbaHotel.Models.Exceptions;
 
 namespace FrbaHotel.Models.Reservas
 {
@@ -17,5 +18,23 @@ namespace FrbaHotel.Models.Reservas
         public String usuario { get; set; } //[nvarchar](30)
         public Reserva reserva { get; set; }
 
+        public override void preSave()
+        {
+            if (String.IsNullOrEmpty(motivo))
+            {
+                throw new ValidationException("El motivo es obligatorio");
+            }
+
+            if (String.IsNullOrEmpty(fecha))
+            {
+                throw new ValidationException("La fecha es obligatoria");
+            }
+
+            if (String.IsNullOrEmpty(usuario))
+            {
+                throw new ValidationException("El usuario es obligatorio");
+            }
+
+        }
     }
 }
