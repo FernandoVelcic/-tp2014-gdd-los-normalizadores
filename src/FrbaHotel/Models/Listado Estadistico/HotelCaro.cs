3e41133a369889ab7b1874708763a12ba35ab72d
Nicolas Azrak <azrak.nicolas@gmail.com>
2014-11-29 14:58:58 -0300

    Correccion estadistica

--- src/FrbaHotel/Models/Listado Estadistico/HotelCaro.cs
+++ src/FrbaHotel/Models/Listado Estadistico/HotelCaro.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using FrbaHotel.Database_Helper;
 
 namespace FrbaHotel.Models.Listado_Estadistico
 {
     class HotelCaro
     {
 
+        public Hotel Hotel
+        {
+            get { return EntityManager.getEntityManager().findById<Hotel>(Convert.ToInt64(hotel_id)); }
+        }
+
+        [System.ComponentModel.Browsable(false)]
         public int hotel_id { get; set; }
         public int total { get; set; }
 
